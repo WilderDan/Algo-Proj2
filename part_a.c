155cc5cda8cb94d17839416a09a0f8487909d87f
Dan Wilder <dan.lee.wilder@gmail.com>
2015-10-17 21:00:05 +0000

    Start part A

--- part_a.c
+++ part_a.c
@@ -9,32 +9,69 @@
  *      Program satisfying part A of project #2.
  */
  
- #include "array_utilities.h"
- #include "sort_algorithms.h"
- #include <stdlib.h>
- #include <time.h>
+#include "array_utilities.h"
+#include "sort_algorithms.h"
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
  
- #define SIZE    30
- #define MIN_VAL 1
- #define MAX_VAL 9999
- 
- int main() {
-     
-     int arr[SIZE];
-     
-     srand(time(NULL));
+#define MIN_VAL             1
+#define MAX_VAL             9999
 
-     printf("TESTING mergeSort...\n");
-     randomInitialize(arr, SIZE, MIN_VAL, MAX_VAL);
-     fprintArray(stdout, arr, SIZE);
-     mergeSort(arr, 0, SIZE - 1);
-     fprintArray(stdout, arr, SIZE);
+#define FILE_BIGGER         "bigger_file"
+#define SIZE_BIGGER         100
+
+#define FILE_SMALLER        "smaller_file"
+#define SIZE_SMALLER        10
+ 
+const int N_SIZES[] = {50, 250, 1250};
+const int M_SIZES[] = {25, 50, 100};
+ 
+int main() {
+    int A[SIZE_BIGGER];
+    int B[SIZE_SMALLER];
+    FILE *fpBig;
+    FILE *fpSmall;
+    
+    srand(time(NULL));
+
+    // Show that mergeSort works
+    printf("%d Unsorted Elements:\n", SIZE_BIGGER);
+    randomInitialize(A, SIZE_BIGGER, MIN_VAL, MAX_VAL);
+    fprintArray(stdout, A, SIZE_BIGGER);
      
-     printf("TESTING quickSort...\n");
-     randomInitialize(arr, SIZE, MIN_VAL, MAX_VAL);
-     fprintArray(stdout, arr, SIZE);
-     quickSort(arr, 0, SIZE - 1);
-     fprintArray(stdout, arr, SIZE);
-     
-     return 0;
+    printf("mergeSort...\n");
+    mergeSort(A, 0, SIZE_BIGGER - 1);
+    fprintArray(stdout, A, SIZE_BIGGER);
+    
+    // Erase current contents
+    memset(A, 0, SIZE_BIGGER * sizeof(int));
+    
+    // Retrieve larger array from file; ASSUME sorted
+    fpBig = fopen(FILE_BIGGER, "r");
+    if (fpBig == NULL) {
+        fprintf(stderr, "Error opening file!\n");
+        exit(1);
+    }
+    
+    readArrayFile(fpBig, A, SIZE_BIGGER);
+    fclose(fpBig);
+    
+    // Retrieve smaller array from file
+    fpSmall = fopen(FILE_SMALLER, "r");
+    if (fpSmall == NULL) {
+        fprintf(stderr, "Error opening file!\n");
+        exit(1);
+    }
+    
+    readArrayFile(fpSmall, B, SIZE_SMALLER);
+    fclose(fpSmall);
+    
+    printf("Bigger sorted file contents:\n");
+    fprintArray(stdout, A, SIZE_BIGGER);
+    
+    printf("Smaller unsorted file contents:\n");
+    fprintArray(stdout, B, SIZE_SMALLER);
+    
+    return 0;
  }
