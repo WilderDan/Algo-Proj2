6f477367b5503ac6beb883aca5ddecbfec79c5f4
Dan Wilder <dan.lee.wilder@gmail.com>
2015-10-20 23:12:12 +0000

    Add medianOfThreeSelect

--- part_b.c
+++ part_b.c
@@ -21,13 +21,15 @@
 #define NUM_TRIALS          10
 
 int basicSelect(int *arr, int lo, int hi);
+int medianOfThreeSelect(int *arr, int lo, int hi);
+int medianOfFiveSelect(int *arr, int lo, int hi);
 
 int main() {
     int arr[DEMO_SIZE];
     
     srand(time(NULL));
     randomInitialize(arr, DEMO_SIZE, MIN_VAL, MAX_VAL);
-    quickSort(arr, 0, DEMO_SIZE - 1, basicSelect);
+    quickSort(arr, 0, DEMO_SIZE - 1, medianOfThreeSelect);
     fprintArray(stdout, arr, DEMO_SIZE);
 
     return 0;
@@ -35,4 +37,17 @@ int main() {
 
 int basicSelect(int *arr, int lo, int hi) {
     return hi - 1;
+}
+
+int medianOfThreeSelect(int *arr, int lo, int hi) {
+    int mid = (hi + lo) / 2;
+    
+    int space[3] = {arr[lo], arr[mid], arr[hi]};
+    mergeSort(space, 0, 2);
+    
+    return (space[1] == arr[lo]) ? lo : ((space[1] == arr[mid]) ? mid : hi); 
+}
+
+int medianOfFiveSelect(int *arr, int lo, int hi) {
+   return -1; 
 }
